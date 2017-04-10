ff5f0954933d1eacdb6d5ea9fc3a3e80e6cf5383
John P <john.pickett@gmail.com>
2017-04-10 11:15:10 -0500

    added ingredients and cuisine, working on css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408180937) do
+ActiveRecord::Schema.define(version: 20170410154430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170408180937) do
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "ingredients"
+    t.string   "cuisine"
   end
 
 end
