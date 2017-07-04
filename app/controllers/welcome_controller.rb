42fdba05088251aff04cef52de2c0119a7aa4894
John P <john.pickett@gmail.com>
2017-07-04 16:44:04 -0500

    welcome view updates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
     @chicken = Recipe.where(main: "Chicken")
     @beef = Recipe.where(main: "Ground Beef")
     @pork = Recipe.where(main: "Pork")
+    @other = Recipe.where.not(main: ["Chicken", "Ground Beef", "Pork"])
   end
 end
